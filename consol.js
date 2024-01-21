51dce50ccacef305ca52867e4ed728e2cd3ab279
Armando VL <103782831+iZOXVL@users.noreply.github.com>
2024-01-21 17:23:33 -0600

    Update consol.js

--- consol.js
+++ consol.js
@@ -1,3 +1,9 @@
 console.log("hello horld");
 console.log("hello horld");
+console.log("hello horld");
+console.log("hello horld");
+console.log("hello horld");
+console.log("hello horld");
+console.log("hello horld");
+console.log("hello horld");
 //Funcion de flecha lol
